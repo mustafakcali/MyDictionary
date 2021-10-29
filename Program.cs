03ecd409602d9bc7f9afbff5810c19551e437487
mustafakcali <93216857+mustafakcali@users.noreply.github.com>
2021-10-29 22:35:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Mustafa");
+            isimler.Add("Umut");
+            isimler.Add("Berk");
+            Console.WriteLine(isimler.Count);
+        }
+    }
+    class MyList<T>
+    {
+        T[] items;
+        T[] tempArray;
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+        }
+
+        public int Count
+        {
+            get { return items.Length; }
+
+        }
+
+
+    }
+}
